362663024345bdcb36b3bbdac9812e0ae7988475
Mark Cruz <markaycruz@gmail.com>
2024-04-04 00:41:54 -0400

    integrate messages to DB

--- frontend/screens/MessagesScreen.js
+++ frontend/screens/MessagesScreen.js
@@ -1,36 +1,78 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SelectMessageScreen from './SelectMessageScreen';
-import MessageLogsScreen from './MessageLogsScreen';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Button,
+  View,
+  ActivityIndicator,
+  Text,
+} from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SelectMessageScreen from "./SelectMessageScreen";
+import MessageLogsScreen from "./MessageLogsScreen";
+import axios from "axios";
+import Constants from "expo-constants";
 
 const Stack = createNativeStackNavigator();
 
-export default function MessagesScreen({route, navigation}) {
-  // const [groupChats, setGroupChats] = useState([])
-  // const [dms, setDms] = useState([])
+export default function MessagesScreen({ route, navigation }) {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const backendEndpoint = Constants.expoConfig.extra.backendEndpoint;
 
-  // useEffect(() => {
-  //   setGroupChats([
-  //     { id: 1, name: "Group Chat 1" },
-  //     { id: 2, name: "Group Chat 2" },
-  //     { id: 3, name: "Group Chat 3" },
-  //   ]);
-  // }, []);
+  const LoadingScreen = () => {
+    return (
+      <View style={styles.loading}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text>Loading...</Text>
+      </View>
+    );
+  };
+
+  useEffect(() => {
+    console.log("attempting to fetch users");
+    axios
+      .get(`http://${backendEndpoint}/get_user_list`)
+      .then((response) => {
+        setUsers(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error retrieving users:", error);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <Stack.Navigator initialRouteName="SelectMessage">
-      <Stack.Screen name="SelectMessage" component={SelectMessageScreen} //initialParams={{ 'groupChats': groupChats, 'setGroupChats': setGroupChats, 'dms': dms, 'setDms': setDms}}
+      <Stack.Screen
+        name="SelectMessage"
+        component={
+          loading
+            ? LoadingScreen
+            : () => (
+                <SelectMessageScreen navigation={navigation} users={users} />
+              )
+        }
+        initialParams={{ users: users }} // Set initialParams here
         options={({ route, navigation }) => ({
-          title: 'Messages',
+          title: "Messages",
           headerRight: () => (
-            <Button title="Forward" 
-              onPress={() => navigation.push('MessageLogs', { chatDetails: {id: "lol", name: "Forward button was hit"} })}
+            <Button
+              title="Forward"
+              onPress={() =>
+                navigation.push("MessageLogs", {
+                  chatDetails: { id: "lol", name: "Forward button was hit" },
+                })
+              }
             />
           ),
         })}
       />
-      <Stack.Screen name="MessageLogs" component={MessageLogsScreen} />
+      <Stack.Screen
+        name="MessageLogs"
+        component={MessageLogsScreen}
+        initialParams={{ users: users }}
+      />
     </Stack.Navigator>
   );
 }
@@ -38,15 +80,20 @@ export default function MessagesScreen({route, navigation}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   input: {
     height: 40,
     margin: 12,
     borderWidth: 1,
     padding: 10,
-    width: '80%',
+    width: "80%",
+  },
+  loading: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
