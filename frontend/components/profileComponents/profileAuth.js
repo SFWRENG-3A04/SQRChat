29a6c0f56eac275208891496e50b7522e519d00e
Arjun Karthik <77539979+arjunkarthik123@users.noreply.github.com>
2024-04-05 01:41:51 -0400

    created functionality to change a user's password upon request

--- frontend/components/profileComponents/profileAuth.js
+++ frontend/components/profileComponents/profileAuth.js
@@ -1,27 +1,25 @@
-import React, { useState } from "react";
-import {TouchableOpacity,StyleSheet, View,Text,TextInput,Button,} from "react-native";
-import { getAuth, updatePassword } from "firebase/auth";
+import React, { useState, useRef } from "react";
+import {StyleSheet, View,Text,TextInput,Button} from "react-native";
+import { ScrollView, Keyboard } from 'react-native';
+import { getAuth, updatePassword, reauthenticateWithCredential, EmailAuthProvider } from "firebase/auth";
 
 const auth = getAuth();
 
 const user = auth.currentUser;
-const emailName = user.email;
-const username = emailName.split('@')[0];
+const emailName = user.email; //Parsing user email 
+const username = emailName.split('@')[0]; //Parsing the beginning of the email to act as a username
 
 const profileAuth = () => {
   const [currentPassword, setCurrentPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
-  const handleCancel = () => {
-    // Implement cancel action
-  };
 
-  const handleSave = () => {
-    // Implement save action
-  };
-
-  const handleChangePassword = async () => {
+  const scrollViewRef = useRef(null);
+  
+  const handleChangePassword = async (newPassword) => {
+    const credential = EmailAuthProvider.credential(user.email, currentPassword);
     if (user) {
       try {
+        await reauthenticateWithCredential(user, credential);
         await updatePassword(user, newPassword);
         console.log("Password updated successfully");
       } catch (error) {
@@ -30,41 +28,56 @@ const profileAuth = () => {
     } else {
       console.error("No user is currently signed in");
     }
+    scrollViewRef.current.scrollToEnd({ animated: true });
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Username</Text>
-      <View style={styles.section}>
-        <Text style={styles.value}>{username}</Text>
-      </View>
-      <Text style={styles.label}>Email</Text>
-      <View style={styles.section}>
-        <Text style={styles.value}>{emailName}</Text>
-      </View>
-      <Text style={styles.label}>Change Password</Text>
-      <View style={[styles.section, styles.changePasswordBox]}>
-        <View style={styles.inputContainer}>
-          <Text style={styles.label}>Current Password</Text>
-          <TextInput
-            style={styles.input}
-            secureTextEntry
-            value={currentPassword}
-            onChangeText={(text) => setCurrentPassword(text)}
-          />
-          <Text style={styles.label}>New Password</Text>
-          <TextInput
-            style={styles.input}
-            secureTextEntry
-            value={newPassword}
-            onChangeText={(text) => setNewPassword(text)}
-          />
+    <ScrollView
+      ref={scrollViewRef}
+      contentContainerStyle={styles.scrollViewContent}
+      keyboardShouldPersistTaps="handled"
+      >
+      <View style={styles.container}>
+        <Text style={styles.label}>Username</Text>
+        <View style={styles.section}>
+          <Text style={styles.value}>{username}</Text>
         </View>
-        <View style={styles.buttonContainer}>
-          <Button title="Update" color="#4B8DF7" />
+        <Text style={styles.label}>Email</Text>
+        <View style={styles.section}>
+          <Text style={styles.value}>{emailName}</Text>
+        </View>
+        <Text style={styles.label}>Change Password</Text> 
+        <View style={[styles.section, styles.changePasswordBox]}>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Current Password</Text> 
+            <TextInput
+              style={styles.input}
+              secureTextEntry
+              value={currentPassword}
+              onChangeText={(text) => setCurrentPassword(text)}
+            />
+            <Text style={styles.label}>New Password</Text>
+            <TextInput
+              style={styles.input}
+              secureTextEntry
+              value={newPassword}
+              onChangeText={(text) => setNewPassword(text)}
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button
+              title="Update"
+              color="#4B8DF7"
+              onPress={() => {
+                handleChangePassword(newPassword);
+                Keyboard.dismiss(); // Dismiss keyboard after pressing "Update"
+              }
+            }
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -117,18 +130,6 @@ const styles = StyleSheet.create({
   buttonText: {
     fontWeight: "bold",
     textAlign: "center",
-  },
-  saveButton: {
-    backgroundColor: "blue",
-  },
-  cancelButton: {
-    backgroundColor: "grey",
-  },
-  saveButtonText: {
-    color: "white",
-  },
-  cancelButtonText: {
-    color: "white",
   },
 });
 
