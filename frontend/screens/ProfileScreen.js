2db0a733d9911152d9036382bb738bab2b740d7a
Arjun Karthik <77539979+arjunkarthik123@users.noreply.github.com>
2024-04-07 00:43:34 -0400

    deleted some unused code

--- frontend/screens/ProfileScreen.js
+++ frontend/screens/ProfileScreen.js
@@ -1,5 +1,5 @@
-import React, {useRef} from 'react';
-import { StyleSheet, Text, View, TouchableWithoutFeedback, KeyboardAvoidingView, ScrollView, Keyboard  } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, TouchableWithoutFeedback, Keyboard  } from 'react-native';
 import ProfileHeader from '../components/profileComponents/profileHeader';
 import ProfileAuth from '../components/profileComponents/profileAuth';
 
@@ -9,19 +9,11 @@ export default function ProfileScreen({ navigation, user }) {
     console.log(isAvailable ? 'Available' : 'Not available');
   };
 
-  const scrollViewRef = useRef(null);
-
-  const handleTextInputFocus = () => {
-    if (scrollViewRef.current) {
-      scrollViewRef.current.scrollToEnd({ animated: true });
-    }
-  };
-
   return (
       <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
         <View style={styles.container}>
           <ProfileHeader onToggleAvailability={onToggleAvailability} />
-          <ProfileAuth user={user} onTextInputFocus={handleTextInputFocus}/>
+          <ProfileAuth user={user}/>
         </View>
      </TouchableWithoutFeedback>
   );
